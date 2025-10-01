c2e5e91b78a71d8b9f0233429210f2ef900cfbf3
genex.mustakim <mustakim.hayder@genexinfosys.com>
2025-10-01 15:06:21 +0600

    fix: URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_BASE_URL: 'http://localhost:9094/api',
-    NEXT_PUBLIC_API_VERSION: 'v1',
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_API_VERSION: process.env.NEXT_PUBLIC_API_VERSION || 'v1',
   },
   images: {
     remotePatterns: [
